c6dc338db2d185b2220fee26dba902125097f7c0
mkoutroumpas <mkoutroumpas@gmail.com>
2017-10-05 17:29:29 +0300

    Proof of Concept (POC) for database CRUD

--- DataAccess/src/com/mk/edu/DataAccessServlet.java
+++ DataAccess/src/com/mk/edu/DataAccessServlet.java
@@ -6,8 +6,12 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
@@ -31,7 +35,7 @@ public class DataAccessServlet extends HttpServlet {
 	
 	private String _DatabaseName = "";
 	private String _SchemaName = "";
-	
+	private DataSource _DataSource = null;
 	private TransactionDAO _TransactionDAO;
 	
 	private static final long serialVersionUID = 1L;
@@ -50,11 +54,16 @@ public class DataAccessServlet extends HttpServlet {
 		
 		this._Logger.info("Database to connect to: " + this._DatabaseName);
 		
-		try {
+		this._doInitTests();
+		
+		/*try {
             InitialContext ctx = new InitialContext();
             DataSource ds = (DataSource) ctx.lookup("java:comp/env/jdbc/" + this._DatabaseName);
-            this._TransactionDAO = new TransactionDAO(ds, this.getCreateTransactionsDB_Script());
-            this._Logger.info("Database initialization successful");
+            if (ds != null)
+            	this._Logger.info("Database lookup successful");
+            this._TransactionDAO = new TransactionDAO(ds, this.getCreateTransactionsDB_Script_Hardcoded(this._SchemaName), this._SchemaName);
+            if (this._TransactionDAO != null)
+            	this._Logger.info("Datasource initialization successful");
 		} 
 		catch (IOException ioex) {
 			this._Logger.error(ioex.getMessage());
@@ -67,19 +76,96 @@ public class DataAccessServlet extends HttpServlet {
 		catch (NamingException e) {
 			this._Logger.error(e.getMessage());
             throw new ServletException(e);
-        }
+        }*/
 	}
 	
+	private void _doInitTests() throws ServletException {
+		Connection connection = null;
+		try {
+			InitialContext ctx = new InitialContext();
+			this._DataSource = (DataSource) ctx.lookup("java:comp/env/jdbc/" + this._DatabaseName);
+			connection = this._DataSource.getConnection();
+			try {
+				connection.prepareStatement("DROP TABLE Transaction").execute();
+				this._Logger.info("Table Transaction deleted successfully");
+			} catch (Exception e) { this._Logger.error("Error deleting Transaction table: " + e.getMessage()); }
+			try {
+				connection.prepareStatement("CREATE COLUMN TABLE Transaction " +
+					"(ID INTEGER, " +
+					"TransactionValue DECIMAL, " +
+					"TransactionCode VARCHAR(255), " +
+					"PRIMARY KEY (ID))").execute();
+				this._Logger.info("Table Transaction created successfully");
+			} catch (Exception e) { this._Logger.error("Error creating Transaction table: " + e.getMessage()); }
+			try {
+				connection.prepareStatement("INSERT INTO Transaction VALUES (1, 0.45, SYSUUID)").execute();
+				this._Logger.info("Row inserted successfully into table Transaction");
+			} catch (Exception e) { this._Logger.error("Error inserting row in Transaction table: " + e.getMessage()); }
+			
+		}
+		catch (Exception e) {
+			this._Logger.error(e.getMessage());
+            throw new ServletException(e);
+        }
+		finally {
+			if (connection != null){
+				try {
+					connection.close();
+				} 
+				catch (SQLException sqex) { throw new ServletException(sqex); }
+			}
+		}
+	}
+
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ArrayList<Transaction> list = new ArrayList<Transaction>();
+		Connection connection = null;
+		try {
+			connection = this._DataSource.getConnection();
+            PreparedStatement pstmt = connection.prepareStatement("SELECT * FROM Transaction");
+            ResultSet rs = pstmt.executeQuery();
+            while (rs.next()) {
+            	Transaction t = new Transaction();
+                t.setID(rs.getLong(1));
+                t.setTransactionValue(rs.getDouble(2));
+                t.setTransactionCode(rs.getString(3));
+                list.add(t);
+            }
+            
+            this._Logger.info("Successfully fetched " + list.size() + " entities");
+        } 
+		catch (Exception ex) {
+			this._Logger.error(ex.getMessage());
+			throw new ServletException(ex);
+		}
+        finally {
+            if (connection != null) {
+				try {
+					connection.close();
+				} catch (SQLException e) {
+					this._Logger.error(e.getMessage());
+					throw new ServletException(e);
+				}
+            }
+        }
 		
-		ArrayList<Transaction> _V = null;
+		if (list.size() > 0) {
+			response.setContentType(this._JSON_MIMEType);
+			response.getWriter().println(new Gson().toJson(list));
+			return;
+		}
+		
+		this._Logger.info("No entities fetched");
+		response.getWriter().println("No entities fetched");
+		
+		/*ArrayList<Transaction> _V = null;
 				
 		try {
 			_V = _TransactionDAO.getTransactions(null);
-		} 
+		}
 		catch (SQLException e) {
 			this._Logger.error(e.getMessage());
 			throw new ServletException(e);
@@ -92,22 +178,62 @@ public class DataAccessServlet extends HttpServlet {
 		}
 		
 		this._Logger.info("No entities fetched");
-		response.getWriter().println("No entities fetched");
+		response.getWriter().println("No entities fetched");*/
 	}
 	
 	private String getCreateTransactionsDB_Script() throws IOException {
-		String _ScriptFile = "/documents/_Create_TransactionsDB.sql";
+		String _ScriptFile = "_Create_TransactionsDB.sql";
 		InputStream _Create_TransactionsDB = this.getServletContext().getResourceAsStream(_ScriptFile);
-		this._Logger.info("Successfully loaded file " + _ScriptFile);
-		BufferedReader _Reader = new BufferedReader(new InputStreamReader(_Create_TransactionsDB));
-        StringBuilder _SB = new StringBuilder();
-        String _Line;
-        while ((_Line = _Reader.readLine()) != null) 
-        	_SB.append(_Line);
-        _Reader.close();
-        
-        this._Logger.info("Successfully read contents of file " + _ScriptFile);
-        return _SB.toString();
+		
+		//Thread.currentThread().getContextClassLoader().getResourceAsStream(name)
+		
+		this._Logger.info("this.getServletContext() is null: " + (this.getServletContext() == null));
+		this._Logger.info("_Create_TransactionsDB is null: " + (_Create_TransactionsDB == null));
+		
+		if (_Create_TransactionsDB != null) {
+			this._Logger.info("Successfully loaded file " + _ScriptFile);
+			
+			Scanner _Scanner = new Scanner(_Create_TransactionsDB).useDelimiter("\\A");
+			String _ret = _Scanner.hasNext() ? _Scanner.next() : "";
+	        _Scanner.close();
+	        
+	        this._Logger.info("Successfully read contents of file " + _ScriptFile);
+	        return _ret;
+		}
+		
+		this._Logger.error("Error loading file " + _ScriptFile);
+		return null;
+	}
+	
+	private String getCreateTransactionsDB_Script_Hardcoded(String schemaname) throws IOException {
+		if (schemaname == null) schemaname = "";
+		return "DROP TABLE " + schemaname + ".Transaction;" +
+			"CREATE COLUMN TABLE " + schemaname + ".Transaction (" +
+				"ID INTEGER," +
+				"TransactionValue DECIMAL," +
+				"TransactionCode VARCHAR(255)," +
+				"PRIMARY KEY (ID));" +
+				" " +
+				"INSERT INTO " + schemaname + ".Transaction VALUES (1, 0.45, SYSUUID);  " +
+				"INSERT INTO " + schemaname + ".Transaction VALUES (2, 23.3342, SYSUUID);  " +
+				"INSERT INTO " + schemaname + ".Transaction VALUES (3, 1.23, SYSUUID);  " +
+				"INSERT INTO " + schemaname + ".Transaction VALUES (4, 0, SYSUUID);  " +
+				"INSERT INTO " + schemaname + ".Transaction VALUES (5, 15.01, SYSUUID); " + 
+				"INSERT INTO " + schemaname + ".Transaction VALUES (6, 7, SYSUUID);  " +
+				"INSERT INTO " + schemaname + ".Transaction VALUES (7, 2.223, SYSUUID);  " +
+				"INSERT INTO " + schemaname + ".Transaction VALUES (8, 83.234, SYSUUID);  " +
+				"INSERT INTO " + schemaname + ".Transaction VALUES (9, 9.0023, SYSUUID);  " +
+				"INSERT INTO " + schemaname + ".Transaction VALUES (10, 1.001, SYSUUID);  " +
+				"INSERT INTO " + schemaname + ".Transaction VALUES (11, 1.004, SYSUUID); " + 
+				"INSERT INTO " + schemaname + ".Transaction VALUES (12, 0, SYSUUID);  " +
+				"INSERT INTO " + schemaname + ".Transaction VALUES (13, 0, SYSUUID);  " +
+				"INSERT INTO " + schemaname + ".Transaction VALUES (14, 63.03, SYSUUID);  " +
+				"INSERT INTO " + schemaname + ".Transaction VALUES (15, 14.933, SYSUUID);  " +
+				"INSERT INTO " + schemaname + ".Transaction VALUES (16, 0.3409, SYSUUID);  " +
+				"INSERT INTO " + schemaname + ".Transaction VALUES (17, -23.34, SYSUUID);" +  
+				"INSERT INTO " + schemaname + ".Transaction VALUES (18, 0, SYSUUID);  " +
+				"INSERT INTO " + schemaname + ".Transaction VALUES (19, -4.009, SYSUUID);" +
+				"INSERT INTO " + schemaname + ".Transaction VALUES (20, 1.0231, SYSUUID);";
 	}
 	
 	/**
--- DataAccess/src/com/mk/edu/TransactionDAO.java
+++ DataAccess/src/com/mk/edu/TransactionDAO.java
@@ -17,11 +17,14 @@ public class TransactionDAO {
 	
 	private final DataSource _DataSource;
 	private final String _TableName = Transaction.class.getSimpleName();
-	private final String _SQLCreateStatement;
 	
-	public TransactionDAO(DataSource dataSource, String sqlcreatestatement) throws SQLException {
+	private String _SchemaName = "";
+	private String _SQLCreateScript;
+	
+	public TransactionDAO(DataSource dataSource, String sqlcreatestatement, String schemaname) throws SQLException {
 		this._DataSource = dataSource;
-		this._SQLCreateStatement = sqlcreatestatement;
+		this._SQLCreateScript = sqlcreatestatement;
+		this._SchemaName = schemaname;
 		
 		this._verifyTable();
 	}
@@ -32,7 +35,7 @@ public class TransactionDAO {
         try {
         	_Connection = this._DataSource.getConnection();
             if (!this._tableExists(_Connection)) 
-                this._createTable(_Connection);
+                this._createTableAndFillWithData(_Connection);
         } 
         finally {
             if (_Connection != null) {
@@ -47,7 +50,8 @@ public class TransactionDAO {
 		
         try {
         	_Connection = this._DataSource.getConnection();
-            PreparedStatement pstmt = _Connection.prepareStatement("SELECT * FROM " + this._TableName + (where == null ? "" : " " + where));
+            PreparedStatement pstmt = _Connection.prepareStatement("SELECT * FROM " + 
+            		(this._SchemaName != null && !this._SchemaName.trim().equals("") ? this._SchemaName + "." : "") + this._TableName + (where == null ? "" : " " + where));
             ResultSet rs = pstmt.executeQuery();
             ArrayList<Transaction> list = new ArrayList<Transaction>();
             while (rs.next()) {
@@ -67,25 +71,44 @@ public class TransactionDAO {
         }
 	}
 	
-	private void _createTable(Connection connection) throws SQLException {
-		if (connection != null && this._SQLCreateStatement != null) {
-			connection.prepareStatement(this._SQLCreateStatement).executeUpdate();
-			this._Logger.info("Table " + this._TableName + " created successfully");
+	private void _createTableAndFillWithData(Connection connection) throws SQLException {
+		if (connection != null && this._SQLCreateScript != null && this._SQLCreateScript.trim() != "") {
+			String[] _Statements = this._SQLCreateScript.split(";");
+			for (String st : _Statements) {
+				this._Logger.info("Executing statement: \"" + st + "\"");
+				try {
+					connection.prepareStatement(st).execute();
+					this._Logger.info("Statement \"" + st + "\" executed successfully");
+				}
+				catch (SQLException sqex) {
+					this._Logger.error("Error executing statement \"" + st + "\": " + sqex.getMessage());
+				}
+			}
 		}
 	}
 
 	private boolean _tableExists(Connection connection) throws SQLException {
 		if (connection == null) return false;
+		
 		DatabaseMetaData _DatabaseMetaData = connection.getMetaData();
-        ResultSet rs = _DatabaseMetaData.getTables(null, null, this._TableName, null);
-        while (rs.next()) {
-            String _Name = rs.getString("TABLE_NAME");
-            if (_Name.equals(this._TableName)) {
-            	this._Logger.info("Table " + this._TableName + " found to exist");
-                return true;
-            }
-        }
-        
+		try {
+	        ResultSet rs = _DatabaseMetaData.getTables(null, null, this._TableName, null);
+	        if (rs != null) {
+		        while (rs.next()) {
+		            String _Name = rs.getString("TABLE_NAME");
+		            if (_Name.indexOf(this._TableName) > -1) {
+		            	this._Logger.info("Table " + this._TableName + " found to exist");
+		                return true;
+		            }
+		        }
+	        }
+		}
+		catch (SQLException sqex) {
+			this._Logger.error("Error verifying existence of table: " + 
+					(this._SchemaName != null && !this._SchemaName.trim().equals("") ? this._SchemaName + "." : "") + this._TableName);
+			throw sqex;
+		}
+		
         this._Logger.info("Table " + this._TableName + " not found to exist");
         return false;
 	}
